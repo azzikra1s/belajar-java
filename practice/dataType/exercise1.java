4ac714af219f47eb9ffe4ad7344d0fc9ad8ab45f
azzikra1s <azzikra.ismunandar08@gmail.com>
2025-11-26 09:59:31 +0700

    Refactor input handling and variable declarations in circle area calculation

--- practice/dataType/exercise1.java
+++ practice/dataType/exercise1.java
@@ -11,21 +11,29 @@ import java.util.Scanner;
 
 public class exercise1 {
     public static void main(String[] args) {
-        Scanner inputRadius = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        // ! Declare variable
         int r;
         double phi;
+        double luasLingkaran;
 
-        System.out.println("Enter the radius of the circle: ");
-        r = inputRadius.nextInt();
-        System.out.println("Circle radius: " + r);
+        // * Input radius
+        System.out.println("Program penghitung luas lingkaran, silakan masukkan jari-jari lingkaran: ");
+        r = scanner.nextInt();
 
+        System.out.println("Diketahui jari-jari yang diinput: " + r);
+
+        // * Phi value jika habis dibagi 7 maka phi 22/7, selain itu 3.14
         if (r % 7 == 0) {
             phi = 22.0 / 7.0;
         } else {
             phi = 3.14;
         }
 
-        double luasLingkaran = phi * r * r;
-        System.out.println("Circle area: " + luasLingkaran);
+        // ! Hitung luas lingkaran
+        luasLingkaran = phi * r * r;
+
+        System.out.println("Luas lingkaran adalah: " + luasLingkaran);
     }
 }
