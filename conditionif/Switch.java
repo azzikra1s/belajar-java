b29bdb84d4d27762b70ce8c8ca07cffa39779559
azzikra1s <azzikra.ismunandar08@gmail.com>
2025-12-01 09:15:04 +0700

    Refactor Switch class to implement grading system using modern switch expression

--- conditionif/Switch.java
+++ conditionif/Switch.java
@@ -4,32 +4,22 @@ public class Switch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input your Month (1-12): ");
-        int monthNumber = scanner.nextInt();
+        System.out.print("Input nilai (1-100): ");
+        int nilai = Integer.valueOf(scanner.nextLine());
 
         //* Java 14+ Modern Switch Expression with Yield (Switch Lambda)
-        String monthName = switch (monthNumber) {
-            case 1 -> "January! (Capricorn)";
-            case 2 -> "February! (Aquarius)";
-            case 3 -> "March! (Aries)";
-            case 4 -> "April! (Taurus)";
-            case 5 -> "May! (Gemini)";
-            case 6 -> "June! (Cancer)";
-            case 7 -> "July! (Leo)";
-            case 8 -> "August! (Virgo)";
-            case 9 -> "September! (Libra)";
-            case 10 -> "October! (Scorpio)";
-            case 11 -> "November! (Sagittarius)";
-            case 12 -> "December! (Capricorn)";
-            default -> "Invalid month input (1-12)!";
+        String grade = switch (nilai/10) {
+            case 10, 9 -> "A";
+            case 8 -> "B";
+            case 7 -> "C";
+            case 6 -> "D";
+            default -> {
+                System.out.println("Butuh remedial!");
+                yield "E";
+            }
         };
         
-        if (monthNumber >= 1 && monthNumber < 13) {
-            System.out.println("Your Month is %s, month-%d in the year.".formatted(monthName, monthNumber));
-        } else {
-            System.out.println(monthName);
-        }
-        
+        System.out.println("Grade Anda: " + grade);
 
         //* Traditional Switch Statement
         /*
